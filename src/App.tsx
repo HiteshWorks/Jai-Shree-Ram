5577ec465f135e3d44744c3f34805024d0b93195
HiteshWorks <hiteshpandey442@gmail.com>
2024-01-21 14:49:10 +0530

    A project dedicated to Lord Ram

---
+++ src/App.tsx
@@ -0,0 +1,165 @@
+import { useEffect, useState } from "react";
+import ConfettiExplosion from "react-confetti-explosion";
+import { Show } from "./Show";
+
+function App() {
+  const [color, setColor] = useState("orange");
+  const [opacity, setOpacity] = useState(0);
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState<string>();
+  const [handScale, setHandScale] = useState(1);
+  const [isfirstPage, setIsFirstPage] = useState(true);
+
+  const hexColorCodes = [
+    "#FFDDC1",
+    "#FFEDD8",
+    "#FAE3D9",
+    "#E1D5E7",
+    "#D4EEEC",
+    "#CFE7F5",
+    "#E0E0E0",
+    "#F8F8F8",
+    "#D3D3D3",
+    "#FFCDCD",
+    "#B5EAD7",
+    "#B4EEB4",
+    "#ADD8E6",
+    "#FFD700",
+    "#FFEC8B",
+    "#F0E68C",
+    "#FFE4E1",
+    "#E0FFFF",
+    "#FFFACD",
+    "#E6E6FA",
+    "#F5F5F5",
+    "#FFE4C4",
+    "#FFFAF0",
+    "#FFEBCD",
+    "#F0F8FF",
+    "#FFF5EE",
+    "#F0FFF0",
+    "#F0E68C",
+    "#98FB98",
+    "#FAFAD2",
+    "#AFEEEE",
+    "#E0FFFF",
+    "#DDA0DD",
+    "#8FBC8F",
+    "#FFB6C1",
+    "#DDA0DD",
+    "#F0E68C",
+    "#AFEEEE",
+    "#FFE4C4",
+    "#D3D3D3",
+    "#FFB6C1",
+    "#F08080",
+    "#ADD8E6",
+    "#FFDEAD",
+    "#98FB98",
+    "#FFC0CB",
+    "#E0FFFF",
+    "#20B2AA",
+    "#FFE4E1",
+    "#AFEEEE",
+  ];
+
+  setTimeout(() => {
+    setOpacity(1);
+  }, 4000);
+
+  useEffect(() => {
+    console.log(opacity);
+
+    let intervalColor = setInterval(() => {
+      let rand = Math.floor(Math.random() * hexColorCodes.length);
+      setColor(hexColorCodes[rand]);
+    }, 10);
+
+    return () => {
+      clearInterval(intervalColor);
+    };
+  }, [color]);
+
+  useEffect(() => {
+    const arrowScaleInterval = setInterval(() => {
+      setHandScale((prevHandScale) => (prevHandScale === 1 ? 1.5 : 1));
+    }, 500);
+
+    const intervalCount = setInterval(() => {
+      setCount((prevCount) => (prevCount === 0 ? 1 : 0));
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalCount);
+      clearInterval(arrowScaleInterval);
+    };
+  }, []);
+
+  document.body.style.backgroundColor = color;
+
+  return isfirstPage ? (
+    <>
+      {count === 0 && (
+        <div className="flex ">
+          <div>
+            <ConfettiExplosion />
+          </div>
+          <div className="absolute right-0">
+            <ConfettiExplosion />
+          </div>
+        </div>
+      )}
+
+      <div className="container h-screen w-full">
+        <div
+          className={`text-6xl text-white absolute left-[34%] border-4 border-yellow-300 bg-yellow-300 rounded-lg shadow-xl shadow-gray-400`}
+        >
+          Jai Shree Ram
+        </div>
+
+        <div className="images flex">
+          <img className="opacity-80 h-screen" src="Ram.png" alt="Ram Image" />
+          <img
+            className="absolute right-0 opacity-80 h-screen"
+            src="Ram.png"
+            alt="Ram Image"
+          />
+        </div>
+      </div>
+      <div className="flex-col absolute top-0 left-[38vw]">
+        <input
+          required
+          style={{ transition: "opacity 2s ease-in-out", opacity: opacity }}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="Enter your Name!"
+          value={name}
+          className="text-white text-center absolute left-[-15%] h-12 top-96 w-[30vw]   bg-orange-200 rounded-xl border-4 border-black"
+          type="text"
+        />
+        <div
+          style={{
+            transition: "opacity 2s ease-in-out",
+            opacity: opacity,
+            transform: `scale(${handScale})`,
+          }}
+          className={`text-4xl scale-[${handScale}] absolute top-[72%] left-36`}
+        >
+          👆
+        </div>
+        <button
+          onClick={() => setIsFirstPage(false)}
+          style={{ transition: "opacity 2s ease-in-out", opacity: opacity }}
+          className={`text-white text-4xl   button text-bold bg-orange-200 rounded-3xl border-4  border-yellow-400 h-28 mt-[70vh] hover:scale-125 hover:bg-sky-200 px-14`}
+        >
+          Jai Shree Ram
+        </button>
+      </div>
+    </>
+  ) : (
+    <Show name={name} />
+  );
+}
+
+export default App;
---
+++ src/Show.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import ConfettiExplosion from "react-confetti-explosion";
+
+interface ShowProps {
+  name: string | undefined;
+}
+
+export const Show: React.FC<ShowProps> = ({ name }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setCount((prevCount) => (prevCount === 0 ? 1 : 0));
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <>
+      {count == 0 && (
+        <>
+          <div>
+            <ConfettiExplosion />
+          </div>
+          <div className="absolute right-0">
+            <ConfettiExplosion />
+          </div>
+          <div className="absolute left-[50vw]">
+            <ConfettiExplosion />
+          </div>
+          <div className="absolute bottom-0 left-[50vw]">
+            <ConfettiExplosion />
+          </div>
+          <div className="absolute bottom-0">
+            <ConfettiExplosion />
+          </div>
+          <div className="absolute bottom-0  right-0">
+            <ConfettiExplosion />
+          </div>
+        </>
+      )}
+
+      <div className="flex">
+        <img className="h-screen" src="./public/RamShow.png" alt="Ram pics" />
+        <div className="showContainer shadow-xl shadow-gray-400 text-red-600 p-5 text-2xl leading-relaxed bg-orange-200 absolute top-44 left-[25%] rounded-lg h-[50vh] w-[50vw]">
+          Heartfelt congratulations to everyone on the auspicious occasion of
+          the Shree Ram Mandir inauguration in Ayodhya. May the temple stand as
+          a symbol of unity, peace, and devotion, fostering harmony and
+          spiritual growth. Wishing blessings to all who contributed to this
+          momentous achievement.{" "}
+          <strong className="text-6xl text-orange-400 mt-3 mx-12">
+            🕊️Jai Shri Ram!🙏
+          </strong>
+          <div className="flex mt-4 text-purple-400 justify-end"> From:{name ? name.toUpperCase() : 'RamBhakt'}</div>
+        </div>
+        <img
+          className="h-screen absolute right-0 z-[-1]"
+          src="./public/RamShow.png"
+          alt="Ram pics"
+        />
+      </div>
+    </>
+  );
+};
